e3f15021fa3a591bbea07ba22ce07b937bf2592f
Dipeshjha123 <142777970+Dipeshjha123@users.noreply.github.com>
2023-12-23 17:42:47 +0545

    Add files via upload
    I have made a E-commerce amazon interface clone that showcases the best of web design using HTML, CSS, and Java Script.

---
+++ script.js
@@ -0,0 +1,25 @@
+function scrollToTop() {
+    // Scroll to the top of the page
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+function redirectToAmazon() {
+    // Redirect to Amazon when the logo is clicked
+    window.open('https://www.amazon.com', '_blank');
+}
+
+function redirectToAmazonF() {
+    // Redirect to Amazon when the logo is clicked
+    window.location.href = 'https://www.amazon.com/s?k=work+from+home+fitness&pd_rd_r=d326d810-d207-4adc-9413-0b02235b31c7&pd_rd_w=8Yn3a&pd_rd_wg=6rvix&pf_rd_p=2c73d1ae-9178-422f-ae3b-1b5628bd95bb&pf_rd_r=H900P458X4V62122HSXP&ref=pd_gw_unk' ;
+}
+function scrollToBottom() {
+    // Scroll to the top of the page
+    window.scrollTo({
+        top: document.documentElement.scrollHeight,
+        behavior: 'smooth'
+    });
+}
+
